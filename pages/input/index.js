81700384c8e6e681e896810c94ac18bbaf719993
Afrillian Ramadhan <pingski.ramadhan@gmail.com>
2022-10-02 21:37:25 +0700

    Form Post Function

--- pages/input/index.js
+++ pages/input/index.js
@@ -1,67 +1,69 @@
 import { useKegiatan } from "../../components/Fetch";
 import { useForm } from "react-hook-form";
-import { useState, useMemo } from "react";
-import { Combobox } from "@headlessui/react";
+import pocketbaseEs from "pocketbase";
 
 const InputWasdit = () => {
+  const client = new pocketbaseEs("http://127.0.0.1:8090/");
+
   const { data: nama_kegiatan } = useKegiatan({
     refethOnWindowFocus: false,
   });
 
-  const [selectedData, setSelectedData] = useState(nama_kegiatan[0]);
-  const [search, setSearch] = useState("");
+  const { handleSubmit, register } = useForm();
 
-  const kegiatanFiltered = useMemo(
-    () =>
-      nama_kegiatan?.filter((keg) => {
-        return keg.kegiatan.toLowerCase().includes(search.toLowerCase());
-      }),
-    [search, nama_kegiatan]
-  );
+  const submitDataHandler = async (data) =>
+    client.records.create("realisasi_diseminasi", data);
+
   return (
     <main
       className="container w-full
-     bg-slate-100 mx-auto"
+     bg-slate-100 flex justify-center items-center"
     >
-      <div className="w-2/6 flex justify-center items-center h-screen flex-col gap-y-4">
+      <div className="w-2/6 flex flex-col gap-y-4">
         <h1 className="text-center text-xl font-noto mt-4">Input Realisasi</h1>
-        <form method="post" className="bg-white p-4">
+        <form
+          method="post"
+          className="bg-white p-4 mx-auto"
+          onSubmit={handleSubmit(submitDataHandler)}
+        >
           <div className="flex flex-col">
             <label>Induk Kegiatan</label>
-            <Combobox value={selectedData} onChange={setSelectedData(search)}>
-              <Combobox.Input
-                onChange={(e) => setSearch(e.target.value)}
-                displayValue={(kegiatan) => kegiatan.kegiatan}
-              />
-              <Combobox.Options>
-                {kegiatanFiltered.map((kegiatan) => {
-                  return (
-                    <Combobox.Option key={kegiatan.id} value={kegiatan.id}>
-                      {({ active, selected }) => (
-                        <li
-                          className={`${
-                            active
-                              ? "bg-blue-500 text-white"
-                              : "bg-white text-black"
-                          }`}
-                        >
-                          {kegiatan.kegiatan}({kegiatan.kode_mak})
-                        </li>
-                      )}
-                    </Combobox.Option>
-                  );
-                })}
-              </Combobox.Options>
-            </Combobox>
+            <select
+              className="form-select px-4 py-3 rounded-full"
+              name="kegiatan"
+              {...register("kegiatan")}
+            >
+              <option value={"none"} defaultValue disabled>
+                Pilih Kegiatan
+              </option>
+              {nama_kegiatan?.map((kegiatan) => {
+                return (
+                  <option key={kegiatan.id} value={kegiatan.id}>
+                    {kegiatan.kegiatan}
+                  </option>
+                );
+              })}
+            </select>
           </div>
           <div className="flex flex-col">
             <label>Nama Kegiatan</label>
-            <input type={"text"} className="form-input rounded-md" />
+            <input
+              type={"text"}
+              className="form-input rounded-md"
+              name="nama_kegiatan"
+              {...register("nama_kegiatan")}
+            />
           </div>
           <div className="flex flex-col mt-3">
             <label>Realisasi</label>
-            <input type={"number"} className="form-input rounded-md" />
+            <input
+              type={"number"}
+              className="form-input rounded-md"
+              name="realisaasi"
+              {...register("realisasi")}
+            />
           </div>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </main>
